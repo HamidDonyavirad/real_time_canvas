0c06e8d233634164f3b5314482a0e1f06fa2d74d
Hamid <donyavirad76@gmail.com>
2025-12-06 20:09:58 +0330

    Real time canvas at its simplest

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI,WebSocket
+from fastapi.responses import HTMLResponse
+import os
 
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HTML_PATH = os.path.join(BASE_DIR, "FrontEnd", "index.html")
+
+connections = []
+@app.get("/")
+async def get():
+    with open(HTML_PATH,encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connections.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for connection in connections:
+                if connection != websocket:
+                    await connection.send_text(data)
+    except:
+        connections.remove(websocket)
